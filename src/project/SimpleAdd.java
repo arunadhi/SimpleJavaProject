33d41e463fee1a82edca47a74fd4343f8ba3e593
arunadhi <pradeep.adhi@gmail.com>
2021-02-25 12:12:51 +0530

    code added

---
+++ src/project/SimpleAdd.java
@@ -0,0 +1,13 @@
+package project;
+
+public class SimpleAdd {
+	
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		int c = a+b;
+		System.out.println("Value of C : "+ c);
+		
+	}
+
+}
